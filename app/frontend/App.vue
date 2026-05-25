769849c6de760986a609fb824297abce75e6b0e8
masahito <think.p.hm.0203@gmail.com>
2026-05-25 18:38:53 +0900

    feat: トップ画面のレイアウトの追加及び、共通部品の作成（ヘッダー・フッター）

--- app/frontend/App.vue
+++ app/frontend/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router'
+  import AppFooter from '@/components/AppFooter.vue'
 </script>
 
 <template>
-  <div>
-    <RouterView />
+  <div class="h-screen bg-[#F2F2F5] text-slate-900 flex flex-col max-w-md mx-auto shadow-sm overflow-hidden">
+    <main class="flex-1 w-full overflow-y-auto">
+      <RouterView />
+    </main>
+    <AppFooter />
   </div>
 </template>
