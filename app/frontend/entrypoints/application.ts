f52a85e2232080bf5f8059b28bbdeae207a36815
masahito <think.p.hm.0203@gmail.com>
2026-06-01 14:54:22 +0900

    chore: VConsole導入

--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import App from '../App.vue'
 import router from '../src/router'
-import { createPinia } from 'pinia' 
+import { createPinia } from 'pinia'
+import VConsole from 'vconsole'
 import '../assets/css/main.css'
+
+const params = new URLSearchParams(window.location.search)
+if (params.get('debug') === 'true' && /iPhone/i.test(navigator.userAgent)) {
+  new VConsole()
+}
 
 const app = createApp(App)
 app.use(router)
